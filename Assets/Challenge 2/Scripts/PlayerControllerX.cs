1f8cd604667d9f3d37282277a5c13657f34e0de1
jtpi <jtrinopi@gmail.com>
2023-05-18 22:06:41 +0100

    End of Challenge2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float coldown = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && coldown <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            coldown = 1;
+        }
+        if (coldown >= 0)
+        {
+            coldown -= Time.deltaTime;
+        }
+    }
+}
